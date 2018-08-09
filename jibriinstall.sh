d1e789bb2b7e92aa41a011cc9e4b8854932beb0c
supun19 <supun.12@cse.mrt.ac.lk>
2018-08-09 12:45:38 +0530

    jibri install and remove script

---
+++ jibriinstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+resources/jenkins/build.sh
+resources/jenkins/release.sh Major
+apt install /home/ubuntu/jibri/-ARTIFACTS/jibri_9.0.-1_all.deb
---
+++ removejibir.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+rm -R /tmp/recordings/*
+rm -R /var/log/jitsi/jibri/*
+rm -R /home/ubuntu/jibri/-ARTIFACTS
+apt remove jibri
