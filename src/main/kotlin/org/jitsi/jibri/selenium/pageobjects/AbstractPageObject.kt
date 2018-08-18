616589fab87a26a695525d9045ac3b7a5be118f0
supun19 <supun.12@cse.mrt.ac.lk>
2018-08-18 13:59:54 +0530

    url as paramter

--- src/main/kotlin/org/jitsi/jibri/selenium/pageobjects/AbstractPageObject.kt
+++ src/main/kotlin/org/jitsi/jibri/selenium/pageobjects/AbstractPageObject.kt
@@ -25,8 +25,8 @@ import org.openqa.selenium.remote.RemoteWebDriver
  */
 open class AbstractPageObject(protected val driver: RemoteWebDriver) {
     open fun visit(url: String): Boolean {
-        // driver.get(url)
-        driver.get("https://classroom.meetrix.io/1234?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InN1cHVuX3JveCIsImZ1bGxOYW1lIjoiU3VwdW4gUm94In0.HE3zcFVtV0pymuxsKFvPxVIH6lT9U-8mO70qxo8MHw4")
+        driver.get(url)
+        //driver.get("https://classroom.meetrix.io/1234?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InN1cHVuX3JveCIsImZ1bGxOYW1lIjoiU3VwdW4gUm94In0.HE3zcFVtV0pymuxsKFvPxVIH6lT9U-8mO70qxo8MHw4")
         return true
     }
 }
