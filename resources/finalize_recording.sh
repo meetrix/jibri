39137468c1a65f0af170d98c5e661fd11de9c922
supun19 <supun.12@cse.mrt.ac.lk>
2018-10-10 22:48:52 +0530

    s3 script

--- resources/finalize_recording.sh
+++ resources/finalize_recording.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RECORDINGS_DIR="/tmp/recordings/1231231231/"
+RECORDINGS_DIR=$1
 
 echo "This is a dummy finalize script" > /tmp/finalize.out
 echo "The script was invoked with recordings directory $RECORDINGS_DIR." >> /tmp/finalize.out
