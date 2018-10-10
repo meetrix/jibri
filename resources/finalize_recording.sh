41a452768854949d5fb749b59d01de632063b20b
supun19 <supun.12@cse.mrt.ac.lk>
2018-10-10 22:14:43 +0530

    s3 script

--- resources/finalize_recording.sh
+++ resources/finalize_recording.sh
@@ -6,5 +6,5 @@ echo "This is a dummy finalize script" > /tmp/finalize.out
 echo "The script was invoked with recordings directory $RECORDINGS_DIR." >> /tmp/finalize.out
 echo "You should put any finalize logic (renaming, uploading to a service" >> /tmp/finalize.out
 echo "or storage provider, etc.) in this script" >> /tmp/finalize.out
-
+aws s3 cp --recursive $RECORDINGS_DIR s3://recorder.classroom.tutopiya.com
 exit 0
