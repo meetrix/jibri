d922df4ca7f95206371fe26eb74a591b5440f3a8
supun19 <supun.12@cse.mrt.ac.lk>
2018-10-22 15:27:17 +0530

    remove time from url

--- src/main/kotlin/org/jitsi/jibri/sink/impl/FileSink.kt
+++ src/main/kotlin/org/jitsi/jibri/sink/impl/FileSink.kt
@@ -29,8 +29,8 @@ import java.time.format.DateTimeFormatter
 class FileSink(recordingsDirectory: Path, callName: String, extension: String = "mp4") : Sink {
     val file: Path
     init {
-        val currentTime = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")
+        //val currentTime = LocalDateTime.now()
+        //val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")
         //val filename = "${callName}_${currentTime.format(formatter)}.$extension"
         val filename = "${callName}.$extension"
         file = recordingsDirectory.resolve(filename)
